ac251745441e202e69e14dd70ac1f9c6d958a8ce
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-06-27 16:54:15 +0900

    update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,34 @@
 $(function(){
-  $('.new-message').on('submit', function(e){
+  function buildHTML(message){
+    var html = `<p>test</p>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href
-    // var url = $(this).attr('action')
+    var url = $(this).attr('action')
     $.ajax({
-      url: href,
+      url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('#message_content').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
 })
+
+// $(function(){
+//   $('#new_message').on('submit',function(){
+//     console.log ("test")
+//   })
+// })
+
