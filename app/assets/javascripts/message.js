755319b3ebc83934653d0dfc56baed8a0f9237d4
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-06-28 16:11:40 +0900

    update message.js
    to make submit btn able after sending message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -32,13 +32,14 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+      
     })
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('#message_content').val('');
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight},'fast');
-
+      $(".submit-btn").prop("disabled", false);
       return false;
     })
     .fail(function(){
