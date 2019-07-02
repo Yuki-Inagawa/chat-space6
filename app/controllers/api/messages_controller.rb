5d7305baaf2387a7c669814b0f2f26f41c7deec2
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-07-02 13:45:07 +0900

    @groupの削除

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages.includes(:user).where("group_id", params[:group_id]).where("id >?", params[:last_id])
+    @messages = messages.includes(:user).where("group_id", params[:group_id]).where("id >?", params[:last_id])
   end
 end
